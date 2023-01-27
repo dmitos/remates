1eb94c9b8a4d7bd60aa99b13ab30fc0d49c53442
dmitos <dmitos@users.noreply.github.com>
2023-01-27 15:11:28 -0300

    cliente
    Se agrega el borrar cliente y ver perfil

--- views_clientes.py
+++ views_clientes.py
@@ -25,6 +25,29 @@ def insertar_cliente():
     return redirect("/Clientes")
 
 
-@views_clientes.route("/Agregar_Clientes")
-def agregar_cliente():
-    return render_template("Clientes/agregar_cliente.html")
+@views_clientes.route("/Alta_Clientes")
+def Alta_Cliente():
+    return render_template("Clientes/alta_cliente.html")
+
+
+@views_clientes.route("/Baja_Cliente")
+def Baja_Cliente():
+    return render_template("Clientes/baja_cliente.html")
+
+
+@views_clientes.route("/Borrar_Cliente", methods=['post'])
+def Borrar_cliente():
+    print("se borro cliente tanto")
+    # cursor = mysql.get_db().cursor()
+    # nombre = request.form['nombre']
+    # direccion = request.form['direccion']
+    # telefono = request.form['telefono']
+    # mail = request.form['mail']
+    # cursor.execute('INSERT INTO Clientes(nombre, telefono, direccion) VALUES("%s", "%s", "%s")' % (nombre, telefono, direccion))
+    return redirect("/Clientes")
+
+
+@views_clientes.route("/Perfil_Cliente")
+def Perfil_Cliente():
+    return render_template("Clientes/perfil_cliente.html")
+
