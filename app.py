3548aa50cda234ded56f769fc8c0d8935b637134
dmitos <dmitos@users.noreply.github.com>
2023-01-29 22:31:28 -0300

    se agrego aviso de mensajes y borrados

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import flask
+# importamos flask
 from flask import Flask
 
 '''
@@ -6,7 +6,7 @@ cuando tenga la base hay q
 descomentar el from db import mysql
 
 '''
-# from db import mysql
+from db import mysql
 
 # Agrego los blueprint()
 from views_clientes import views_clientes  # importo el manejador de clientes
@@ -14,6 +14,7 @@ from index import index  # importo el manejador del inicio
 
 # inicio la clase Flask
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # registro los blueprint antes importados y le doy direccion de prefijo
 app.register_blueprint(views_clientes, url_prefix="/")
 app.register_blueprint(index, url_prefix="/")
@@ -35,12 +36,12 @@ Descomentar la base de datos que voy a usar en su momento
 # app.config['MYSQL_DATABASE_PASSWORD'] = ''
 # app.config['MYSQL_DATABASE_DB'] = 'sqremates'
 
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# app.config['MYSQL_DATABASE_PORT'] = 3306
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = ''
-# app.config['MYSQL_DATABASE_DB'] = 'prueba'
-# mysql.init_app(app)
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'dana'
+app.config['MYSQL_DATABASE_DB'] = 'prueba'
+mysql.init_app(app)
 
 # inicio el servidor de pruebas flask
 if __name__ == '__main__':
--- views_clientes.py
+++ views_clientes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, redirect
-# from db import mysql
+from flask import Blueprint, render_template, request, redirect, flash
+from db import mysql
 
 
 views_clientes = Blueprint(__name__, "views_clientes")
@@ -7,21 +7,27 @@ views_clientes = Blueprint(__name__, "views_clientes")
 
 @views_clientes.route("/Clientes")
 def cliente():
-    # cursor = mysql.get_db().cursor()
-    # cursor.execute('SELECT * FROM Clientes')
-    # clientes = cursor.fetchall()
-    # print(clientes)
-    # return render_template("Clientes/clientes.html", clientes=clientes) cuando este la base de datos
-    return render_template("Clientes/clientes.html")
+    base_datos = mysql.get_db()
+    cursor = base_datos.cursor()
+    cursor.execute('SELECT * FROM Clientes')
+    clientes = cursor.fetchall()
+    print(clientes)
+    return render_template("Clientes/clientes.html", clientes=clientes) 
+    # return render_template("Clientes/clientes.html")
 
-@views_clientes.route("/Insertar_Clientes", methods=['post'])
+@views_clientes.route("/Insertar_Clientes", methods=['POST'])
 def insertar_cliente():
-    # cursor = mysql.get_db().cursor()
-    # nombre = request.form['nombre']
-    # direccion = request.form['direccion']
-    # telefono = request.form['telefono']
-    # mail = request.form['mail']
-    # cursor.execute('INSERT INTO Clientes(nombre, telefono, direccion) VALUES("%s", "%s", "%s")' % (nombre, telefono, direccion))
+    base_datos = mysql.get_db()
+    cursor = base_datos.cursor()
+    documento = request.form['documento']
+    nombre = request.form['nombre']
+    direccion = request.form['direccion']
+    telefono = request.form['telefono']
+    dpto = request.form['departamento']
+    mail = request.form['mail']
+    cursor.execute('INSERT INTO Clientes(ClienteDoc, ClienteNombre, ClienteMail, ClienteTelefono, ClienteDireccion, ClienteDepto) VALUES ("%s", "%s", "%s", "%s", "%s", "%s")' % (documento, nombre, mail, telefono, direccion, dpto))
+    base_datos.commit()
+    print(documento, nombre, mail, telefono, direccion, dpto)
     return redirect("/Clientes")
 
 
@@ -30,24 +36,28 @@ def Alta_Cliente():
     return render_template("Clientes/alta_cliente.html")
 
 
-@views_clientes.route("/Baja_Cliente")
-def Baja_Cliente():
-    return render_template("Clientes/baja_cliente.html")
+@views_clientes.route("/Baja_Cliente/<id>/")
+def Baja_Cliente(id):
+    base_datos = mysql.get_db()
+    cursor = base_datos.cursor()
+    cursor.execute('SELECT * FROM Clientes WHERE ClienteId = %s', (id))
+    Cliente = cursor.fetchall()
+    print(cliente)
+    return render_template("Clientes/baja_cliente.html", cliente=Cliente)
 
 
 @views_clientes.route("/Borrar_Cliente", methods=['post'])
 def Borrar_cliente():
-    print("se borro cliente tanto")
-    # cursor = mysql.get_db().cursor()
-    # nombre = request.form['nombre']
-    # direccion = request.form['direccion']
-    # telefono = request.form['telefono']
-    # mail = request.form['mail']
-    # cursor.execute('INSERT INTO Clientes(nombre, telefono, direccion) VALUES("%s", "%s", "%s")' % (nombre, telefono, direccion))
+    base_datos = mysql.get_db()
+    cursor = base_datos.cursor()
+    borrar = request.form['id']
+    print("se borro cliente", (borrar))
+    cursor.execute('DELETE FROM Clientes WHERE ClienteId = %s', (borrar))
+    base_datos.commit()
+    flash('Se borro el cliente')
     return redirect("/Clientes")
 
 
 @views_clientes.route("/Perfil_Cliente")
 def Perfil_Cliente():
     return render_template("Clientes/perfil_cliente.html")
-
